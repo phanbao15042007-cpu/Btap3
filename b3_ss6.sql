e8d949e42d28263f15827a06a857d1d06a095c00
Phan <phanbao15042007@gmail.com>
2026-05-19 10:21:52 +0700

    z

--- b3_ss6.sql
+++ b3_ss6.sql
@@ -1,14 +1,37 @@
-CREATE DATABASE ss06_b3;
-USE ss06_b3;
+CREATE DATABASE ss07_b3;
+USE ss07_b3;
 
--- NOT EXISTS (Dừng sớm): Chỉ cần quét thấy 1 giao dịch trong năm 2024 là nó dừng kiểm tra học viên đó và bỏ qua ngay lập tức Xử lý cực nhanh.
--- NOT IN: Phải quét toàn bộ bảng để tạo ra một "danh sách khổng lồ" chứa tất cả giao dịch 2024 rồi mới đem đi dò từng người Tốn RAM, cực chậm.
+CREATE TABLE Students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
 
-SELECT s.email
-FROM Students s
-WHERE NOT EXISTS (
-    SELECT 1 
-    FROM Payments p
-    WHERE p.student_id = s.id 
-      AND YEAR(p.payment_date) = 2024
+CREATE TABLE Support_Tickets (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    content TEXT NOT NULL,
+    student_id INT,
+    FOREIGN KEY (student_id) REFERENCES Students(id)
+);
+
+INSERT INTO Students (id, full_name, email) VALUES 
+(1, 'Nguyễn Minh Anh', 'minhanh.nguyen@email.com'),
+(2, 'Trần Hoàng Bách', 'bach.tran@email.com'),
+(3, 'Lê Ngọc Diệp', 'diep.le@email.com'),
+(4, 'Phạm Quốc Bảo', 'bao.pham@email.com'),
+(5, 'Vũ Hải Đăng', 'dang.vu@email.com');
+
+INSERT INTO Support_Tickets (content, student_id) VALUES 
+('Lỗi không đăng nhập được vào hệ thống', 1),
+('Quên mật khẩu, nhờ hỗ trợ lấy lại', NULL),
+('Hỏi về lộ trình học Fullstack chuyên nghiệp', NULL),
+('Video bài giảng số 5 bị mất tiếng', 2),
+('Thanh toán học phí nhưng chưa được kích hoạt', 4);
+
+SELECT email, full_name
+FROM Students
+WHERE id NOT IN (
+    SELECT student_id
+    FROM Support_Tickets
+    WHERE student_id IS NOT NULL
 );
